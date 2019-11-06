27475089a83e443e5cbcf604231a69de3a16eed3
CarnunMP <carnun@hotmail.co.uk>
2019-11-06 15:50:56 +0000

    findById() done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,9 +2,16 @@ const db = require('../data/db-config');
 
 module.exports = {
   find,
+  findById,
 }
 
 function find() {
   return db('schemes AS s')
     .select('s.scheme_name');
+}
+
+function findById(id) {
+  return db('schemes AS s')
+  .where({ 's.id': id })
+  .first(); // Ensures resolves to _null_ if illegitimate id.
 }
