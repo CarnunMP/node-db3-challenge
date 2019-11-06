7c8e36cdde2bc91dec2703850875e192a3904c3b
CarnunMP <carnun@hotmail.co.uk>
2019-11-06 17:50:42 +0000

    findSteps(id) done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,6 +3,7 @@ const db = require('../data/db-config');
 module.exports = {
   find,
   findById,
+  findSteps,
 }
 
 function find() {
@@ -14,4 +15,17 @@ function findById(id) {
   return db('schemes AS s')
   .where({ 's.id': id })
   .first(); // Ensures resolves to _null_ if illegitimate id.
+}
+
+function findSteps(id) {
+  return db('steps AS st')
+    .join('schemes AS sc', 'st.scheme_id', 'sc.id')
+    .select(
+      'st.id',
+      'sc.scheme_name',
+      'st.step_number',
+      'st.instructions'
+    )
+    .where({ 'st.scheme_id': id });
+    // Does it matter if ^this is as-is or _'sc.id': id_?
 }
